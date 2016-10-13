d44b5c0b83ee502fca828f4257369f145a60a465
nikklein <objcoding@gmail.com>
2016-10-13 14:36:54 +0100

    Test @Autowired in BlogPostServiceImpl

--- src/main/java/spring/java/service/impl/BlogPostServiceImpl.java
+++ src/main/java/spring/java/service/impl/BlogPostServiceImpl.java
@@ -2,18 +2,30 @@ package spring.java.service.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import spring.java.domain.BlogPost;
+import spring.java.domain.DataSource;
 import spring.java.service.BlogPostService;
+import spring.java.service.EmailService;
 
 public class BlogPostServiceImpl implements BlogPostService {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(BlogPostServiceImpl.class);
 	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private EmailService emailService;
+	
 	@Override
 	public void savePost(BlogPost blogPost) {
 		
+		LOGGER.debug("BlogPostServiceImpl: " + dataSource.getPassword());
 		LOGGER.debug("BlogPostServiceImpl: savePost is called");
+		
+		emailService.sendEmail();
 	}
 
 	 
