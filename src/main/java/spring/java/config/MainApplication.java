b6aad444d54f5ead0bef48e519ec354bb53b5afb
nikklein <objcoding@gmail.com>
2016-10-14 12:04:17 +0100

    Use application-context to define beans

--- src/main/java/spring/java/config/MainApplication.java
+++ src/main/java/spring/java/config/MainApplication.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 import spring.java.domain.BlogPost;
 import spring.java.domain.DataSource;
@@ -18,32 +20,38 @@ public class MainApplication {
 
     public static void main(String[] args) {
 
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(JavaConfig.class);
-
-        // BlogPostService blogPostService =
-        // applicationContext.getBean(BlogPostService.class);
-
-        BlogPostService blogPostService = (BlogPostService) applicationContext.getBean("blogPostService");
-
-        BlogPost blogPost = new BlogPost();
-        blogPost.setId(1L);
-        blogPost.setTitle("JavaSpring MVC Java Config");
-
-        blogPostService.savePost(blogPost);
-
+//        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(JavaConfig.class);
+//
+//        // BlogPostService blogPostService =
+//        // applicationContext.getBean(BlogPostService.class);
+//
+//        BlogPostService blogPostService = (BlogPostService) applicationContext.getBean("blogPostService");
+//
+//        BlogPost blogPost = new BlogPost();
+//        blogPost.setId(1L);
+//        blogPost.setTitle("JavaSpring MVC Java Config");
+//
+//        blogPostService.savePost(blogPost);
+//
+//        DataSource dataSource = applicationContext.getBean(DataSource.class);
+//
+//        LOGGER.debug("dataSource: username " + dataSource.getUsername());
+//        LOGGER.debug("dataSource: password " + dataSource.getPassword());
+//
+//        EmailService emailService = applicationContext.getBean(EmailService.class);
+//
+//        emailService.sendEmail();
+//
+//        ServiceManager serviceManager = applicationContext.getBean(ServiceManager.class);
+//        serviceManager.sendBlogPost(blogPost);
+//
+//        ((ConfigurableApplicationContext) applicationContext).close();
+        
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml");
+        
         DataSource dataSource = applicationContext.getBean(DataSource.class);
-
         LOGGER.debug("dataSource: username " + dataSource.getUsername());
         LOGGER.debug("dataSource: password " + dataSource.getPassword());
-
-        EmailService emailService = applicationContext.getBean(EmailService.class);
-
-        emailService.sendEmail();
-
-        ServiceManager serviceManager = applicationContext.getBean(ServiceManager.class);
-        serviceManager.sendBlogPost(blogPost);
-
-        ((ConfigurableApplicationContext) applicationContext).close();
 
     }
 
