51ddb67f48e05d1bdc12b4b27aa7134c3a6e3b11
nikklein <objcoding@gmail.com>
2016-10-14 10:56:51 +0100

    Update styling

--- src/main/java/spring/java/config/MainApplication.java
+++ src/main/java/spring/java/config/MainApplication.java
@@ -13,36 +13,36 @@ import spring.java.service.EmailService;
 import spring.java.service.ServiceManager;
 
 public class MainApplication {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(MainApplication.class);
 
-	
 	public static void main(String[] args) {
-		
+
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(JavaConfig.class);
-		
-		//BlogPostService blogPostService = applicationContext.getBean(BlogPostService.class);
-		
+
+		// BlogPostService blogPostService =
+		// applicationContext.getBean(BlogPostService.class);
+
 		BlogPostService blogPostService = (BlogPostService) applicationContext.getBean("blogPostService");
-		
+
 		BlogPost blogPost = new BlogPost();
 		blogPost.setId(1L);
 		blogPost.setTitle("JavaSpring MVC Java Config");
-		
+
 		blogPostService.savePost(blogPost);
-		
+
 		DataSource dataSource = applicationContext.getBean(DataSource.class);
-		
+
 		LOGGER.debug("dataSource: username " + dataSource.getUsername());
 		LOGGER.debug("dataSource: password " + dataSource.getPassword());
-		
+
 		EmailService emailService = applicationContext.getBean(EmailService.class);
-		
+
 		emailService.sendEmail();
-		
+
 		ServiceManager serviceManager = applicationContext.getBean(ServiceManager.class);
 		serviceManager.sendBlogPost(blogPost);
-		
+
 		((ConfigurableApplicationContext) applicationContext).close();
 
 	}
